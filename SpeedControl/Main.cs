cf199b00e4bcb59f911abd12d1b32b3edcc7ce90
Tim Potze <timpotze@gmail.com>
2015-10-01 18:37:45 +0200

    Initial commit

---
+++ SpeedControl/Main.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace SpeedControl
+{
+    public class Main : IMod
+    {
+        private GameObject _gameObject;
+
+        #region Implementation of IMod
+
+        public void onEnabled()
+        {
+            if (_gameObject != null)
+                return;
+
+            _gameObject = new GameObject();
+            _gameObject.AddComponent<SpeedControl>();
+        }
+
+        public void onDisabled()
+        {
+            Object.Destroy(_gameObject);
+            _gameObject = null;
+        }
+
+        public string Name
+        {
+            get { return "GuestCam"; }
+        }
+
+        public string Description
+        {
+            get { return Name; }
+        }
+
+        #endregion
+    }
+}
---
+++ SpeedControl/SpeedControl.cs
@@ -0,0 +1,42 @@
+using Parkitect.UI;
+using UnityEngine;
+
+namespace SpeedControl
+{
+    public class SpeedControl : MonoBehaviour
+    {
+        private float _defaultTimeStep;
+        private void Awake()
+        {
+            _defaultTimeStep = Time.fixedDeltaTime;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Update()
+        {
+            if (UIUtility.isInputFieldFocused())
+                return;
+
+            if (Input.GetKeyUp(KeyCode.Alpha4))
+                SetGameSpeed(5);
+            if (Input.GetKeyUp(KeyCode.Alpha5))
+                SetGameSpeed(7);
+            if (Input.GetKeyUp(KeyCode.Alpha6))
+                SetGameSpeed(10);
+        }
+
+        private void SetGameSpeed(float newSpeed)
+        {
+            if (Time.timeScale == newSpeed) return;
+            var oldSpeed = Time.timeScale;
+            Time.timeScale = newSpeed;
+            Time.fixedDeltaTime = newSpeed >= 1.0f ? _defaultTimeStep : _defaultTimeStep * newSpeed;
+            UIEvents.Instance.RaiseOnTimeSpeedChanged(oldSpeed, newSpeed);
+        }
+
+
+        private void OnDestroy()
+        {
+        }
+    }
+}
